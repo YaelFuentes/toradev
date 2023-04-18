dcb5637cc03310a0ade592cfdb605aa710f05d49
navarroJuanIgnacio <n.juanignacio99@gmail.com>
2023-04-18 09:52:09 -0300

    apply dark/sun  icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Logo from './Logo';
+import useThemeSwitcher from './hooks/useThemeSwitcher';
 
-const CustomLink = ({href, title, className=""}) => {
+const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
-  return(
+  return (
     <Link href={href} className={`${className} relative group`}>
       {title}
       <span className={`h-[1px] inline-block w-0 bg-dark 
@@ -16,13 +17,15 @@ const CustomLink = ({href, title, className=""}) => {
 }
 
 const NavBar = () => {
+
+  const [mode, setMode] = useThemeSwitcher();
   return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
       <nav>
-        <CustomLink href="/" title="Home" className='mr-4'/>
-        <CustomLink href="/about" title="About" className='mx-4'/>
-        <CustomLink href="/projects" title="Project" className='mx-4'/>
-        <CustomLink href="/articles" title="Articles" className='ml-4'/>
+        <CustomLink href="/" title="Home" className='mr-4' />
+        <CustomLink href="/about" title="About" className='mx-4' />
+        <CustomLink href="/projects" title="Project" className='mx-4' />
+        <CustomLink href="/articles" title="Articles" className='ml-4' />
       </nav>
       {/* <Logo /> */}
       <nav>
@@ -32,6 +35,14 @@ const NavBar = () => {
         <Link href="/" target={"_blank"}>T</Link>
         <Link href="/" target={"_blank"}>T</Link>
         <Link href="/" target={"_blank"}>T</Link>
+
+        <button onClick={() => setMode(mode === "light" ? "dark" : "light")}
+          className="ml-3 flex items-center justify-center rounded-full p-1"
+        >
+          {
+            mode === "dark" ? <SunIcon className={"fill-dark"} /> : <MoonIcon className={"fill-dark"} />
+          }
+        </button>
       </nav>
       <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo />
