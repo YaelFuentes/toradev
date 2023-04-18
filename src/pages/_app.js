119221bb7db41f5fb198b57330ecdf92b8114c1a
YaelFuentes <yaelfuentes32@gmail.com>
2023-04-18 09:17:41 -0300

    NavBar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import {Montserrat} from "next/font/google"
+import NavBar from "../components/navBar"
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -8,7 +9,8 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${montserrat.variable} font-mont`}>
+    <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+      <NavBar />
       <Component {...pageProps} />
     </main>
   )
