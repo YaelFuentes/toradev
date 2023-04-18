84a25865146dded62af8d2ad2f173fafdf21a6fb
YaelFuentes <yaelfuentes32@gmail.com>
2023-04-18 14:17:39 -0300

    hotfix

---
+++ src/components/NavBar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Logo from './Logo';
+import { LinkedInIcon, TwitterIcon } from './Icons';
+import { motion } from "framer-motion";
+
+<<<<<<< HEAD
+
+=======
+>>>>>>> f7e6306cadb75552b16acadf249ee50c140eb289
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span className={`h-[1px] inline-block w-0 bg-dark 
+      absolute left-0 -bottom-0.5 group-hover:w-full 
+      transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
+    </Link>
+  )
+}
+
+const NavBar = () => {
+
+  const [mode, setMode] = useThemeSwitcher();
+  return (
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+      <nav>
+        <CustomLink href="/" title="Home" className='mr-4' />
+        <CustomLink href="/about" title="About" className='mx-4' />
+        <CustomLink href="/projects" title="Project" className='mx-4' />
+        <CustomLink href="/articles" title="Articles" className='ml-4' />
+      </nav>
+<<<<<<< HEAD
+      <nav className='flex items-center justify-center flex-wrap'>
+        <motion.a href="https://google.com" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mx-3'>
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a href="https://google.com" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mx-3'>
+          <TwitterIcon />
+        </motion.a>
+=======
+      {/* <Logo /> */}
+      <nav>
+        <Link href="/" target={"_blank"}><LinkedInIcon /></Link>
+        <Link href="/" target={"_blank"}><TwitterIcon /></Link>
+         <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+
+        <button onClick={() => setMode(mode === "light" ? "dark" : "light")}
+          className="ml-3 flex items-center justify-center rounded-full p-1"
+        >
+          {
+            mode === "dark" ? <SunIcon className={"fill-dark"} /> : <MoonIcon className={"fill-dark"} />
+          }
+        </button>
+>>>>>>> f7e6306cadb75552b16acadf249ee50c140eb289
+      </nav>
+      <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+        <Logo />
+      </div>
+    </header>
+  )
+}
+
+export default NavBar;
